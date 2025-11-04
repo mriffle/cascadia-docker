fe0e180de47609dd3a1beb884f75ddfb1cdee2de
Michael Riffle <mriffle@uw.edu>
2025-11-03 16:40:48 -0800

    make entrypoint.sh sourceable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
-
 set -e
 
 mkdir -p /tmp/numba-cache
 export NUMBA_CACHE_DIR=/tmp/numba-cache
 
 # Activate the Conda environment
+# shellcheck disable=SC1091
 source /opt/conda/etc/profile.d/conda.sh
 conda activate cascadia_env
 
-exec "$@"
+# Only exec if this script is the main program, not when sourced
+if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
+  exec "$@"
+fi
